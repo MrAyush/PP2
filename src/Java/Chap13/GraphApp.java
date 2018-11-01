81891ee076ab3538585488eafb4c35c40f85c8ec
HATERS\Ayush Gupta <ayushkgupta1998@gmail.com>
2018-11-01 20:48:25 +0530

    Graph done

--- src/Java/Chap13/GraphApp.java
+++ src/Java/Chap13/GraphApp.java
@@ -30,6 +30,7 @@ class Graph {
     // adds the weight to the edge or can say creates a edge with weight 1
     public void addEdge(int u, int v) {
         graph[u][v] = 1;
+        graph[v][u] = 1;
     }
 
     // remove the the given edge
@@ -44,7 +45,7 @@ class Graph {
 
     // returns all the neighbours of the given vertex by traversing
     public List<Integer> neighbours(int vertex) {
-        List<Integer> edges = new ArrayList<Integer>();
+        List<Integer> edges = new ArrayList<>();
         for (int v = 0; v < vertexCount; v++) {
             if (hasEdge(vertex, v)) {
                 edges.add(v);
@@ -61,43 +62,66 @@ class Graph {
                 System.out.print(v + " ");
             }
         }
+        System.out.println();
     }
-	
-	public void bfs() {
-		boolean isVisited[] = new boolean[vertexCount];
-		Arrays.fill(isVisited, false);
-		Queue<Integer> q = new LinkedList<>();
-		q.add(0);
-		isVisited[0] = true;
-		System.out.println("Found : " + 0);
-		while (!q.isEmpty()) {
-			int v1 = q.poll();
-			List<Integer> connection = neighbours(v1);
-			for (Integer v2 : connection) {
-				if (!isVisited[v2]) {
-					q.add(v2);
-					isVisited[v2] = true;
-					System.out.println("Found : " + v2);
-				}
-			}
-		}
-	}
+
+    public void dfs() {
+        boolean[] isVisited = new boolean[vertexCount];
+        Arrays.fill(isVisited, false);
+        for (int v = 0; v < vertexCount; v++)
+            dfs(v, isVisited);
+    }
+
+    private void dfs(int v, boolean[] isVisited) {
+        for (int i = 0; i < vertexCount; i++) {
+            if (hasEdge(v, i) && !isVisited[i]) {
+                System.out.println("Found: " + i);
+                isVisited[i] = true;
+                dfs(i, isVisited);
+            }
+        }
+    }
+
+    public void bfs() {
+        boolean isVisited[] = new boolean[vertexCount];
+        Arrays.fill(isVisited, false);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        isVisited[0] = true;
+        System.out.println("Found : " + 0);
+        while (!q.isEmpty()) {
+            int v1 = q.poll();
+            List<Integer> connection = neighbours(v1);
+            for (Integer v2 : connection) {
+                if (!isVisited[v2]) {
+                    q.add(v2);
+                    isVisited[v2] = true;
+                    System.out.println("Found : " + v2);
+                }
+            }
+        }
+    }
 }
+
 public class GraphApp {
-	public static void main(String[] args) {
-		Graph graph = new Graph(10);
-		graph.addEdge(0,1);
-		graph.addEdge(0,3);
-		graph.addEdge(1,0);
-		graph.addEdge(1,2);
-		graph.addEdge(1,4);
-		graph.addEdge(2,1);
-		graph.addEdge(2,3);
-		graph.addEdge(3,0);
-		graph.addEdge(3,2);
-		graph.addEdge(4,1);
-		graph.addEdge(4,2);
-		graph.printGraph();
-		graph.bfs();
-	}
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 0);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 1);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 0);
+        graph.addEdge(3, 2);
+        graph.addEdge(4, 1);
+        graph.addEdge(4, 2);
+        System.out.println("Graph: ");
+        graph.printGraph();
+        System.out.println("Graph traversing (BFS): ");
+        graph.bfs();
+        System.out.println("Graph traversing (DFS): ");
+        graph.dfs();
+    }
 }
